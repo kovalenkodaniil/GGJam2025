7e84339a1cd37ab674c7ad58b3a92a0e4aa0ccdd
pkudryavcev <pkudryavcev@gear-games.com>
2025-04-12 10:12:11 +0300

    add task reward

---
+++ Assets/_Core/Scripts/Tasks/TaskRewardConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Core.Scripts.Tasks
+{
+    [CreateAssetMenu(fileName = "New Reward", menuName = "Create new Reward")]
+    public class TaskRewardConfig : ScriptableObject
+    {
+        public string id;
+        public List<RewardAttribute> rewardAttributes;
+    }
+    
+    public enum EnumReward
+    {
+        Graphic,
+        Popularity,
+        Quality,
+        Gameplay,
+        Profit
+    }
+    
+    [Serializable]
+    public class RewardAttribute
+    {
+        public EnumReward type;
+        public int value;
+    }
+
+}
